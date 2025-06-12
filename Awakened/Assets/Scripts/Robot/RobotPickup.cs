72d4efb6d0b646d00a96bb9c17baccab691316ca
AnastazijaSirol <119959886+AnastazijaSirol@users.noreply.github.com>
2025-06-12 13:41:08 +0200

    spremanje robota u inventory

---
+++ Awakened/Assets/Scripts/Robot/RobotPickup.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class RobotPickup : MonoBehaviour
+{
+    public GameObject robotIconPrefab;
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            GameObject newIcon = Instantiate(robotIconPrefab);
+            
+            Inventory.Instance.AddItem(newIcon);
+            
+            gameObject.SetActive(false);
+        }
+    }
+}
