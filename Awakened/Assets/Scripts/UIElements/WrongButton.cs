582c2b6177ca7daf5fcb3341765ead01c1f3850b
AnastazijaSirol <119959886+AnastazijaSirol@users.noreply.github.com>
2025-06-14 14:57:22 +0200

    zvuk za krive odgovore

--- Awakened/Assets/Scripts/UIElements/WrongButton.cs
+++ Awakened/Assets/Scripts/UIElements/WrongButton.cs
@@ -2,8 +2,26 @@ using UnityEngine;
 
 public class WrongButton : MonoBehaviour
 {
+    public AudioClip buttonClickSound;
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        if (audioSource == null)
+        {
+            audioSource = gameObject.AddComponent<AudioSource>();
+        }
+    }
+
     public void OnRestartButtonClick()
     {
+        // Reproduciraj zvuk ako postoji
+        if (buttonClickSound != null && audioSource != null)
+        {
+            audioSource.PlayOneShot(buttonClickSound);
+        }
+
         HealthManager[] managers = Object.FindObjectsByType<HealthManager>(FindObjectsSortMode.None);
         foreach (var manager in managers)
         {
