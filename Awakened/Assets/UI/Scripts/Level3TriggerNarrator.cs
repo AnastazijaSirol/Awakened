0366592b0ce82f46ef449a2cf9bcae70dd3cfa2e
MBucaj <143554778+MBucaj@users.noreply.github.com>
2025-07-03 18:35:59 +0200

    ispravke gresaka

--- Awakened/Assets/UI/Scripts/Level3TriggerNarrator.cs
+++ Awakened/Assets/UI/Scripts/Level3TriggerNarrator.cs
@@ -7,10 +7,18 @@ public class Level3TriggerNarrator : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (!hasTriggered && other.CompareTag("Player"))
+        if (hasTriggered)
+        {
+            Debug.Log("Trigger already used, ignoring extra enter.");
+            return;
+        }
+
+        if (other.CompareTag("Player"))
         {
             hasTriggered = true;
+            Debug.Log($"Level 3 narrator triggered by: {other.name} on {gameObject.name}");
             narrator?.StartNarration();
         }
     }
+
 }
